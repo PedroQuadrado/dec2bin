22d42cbfff7f9f8ea25115e16c68fcd95055fe3f
PedroQuadrado <51176756+PedroQuadrado@users.noreply.github.com>
2020-11-19 09:18:47 +0000

    Update app.js

--- app.js
+++ app.js
@@ -55,3 +55,15 @@ function convertToBinary(btn) {
   }
   binString.innerText = tempBin;
 }
+
+//add the option to use the enter key instead of button
+decValue.addEventListener("keyup", function(event) {
+  console.log(event);
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("btn").click();
+  }
+});
